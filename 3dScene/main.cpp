37d4a3e34fb2a4e1b2f13b7673d7b6d16bc87841
Pascal Attwenger <Pascal@Penelope.local>
2014-11-14 14:09:16 +0000

    Initial Commit

---
+++ 3dScene/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3dScene
+//
+//  Created by Pascal Attwenger on 14/11/14.
+//  Copyright (c) 2014 Pascal Attwenger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
